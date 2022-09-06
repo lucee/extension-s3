a92f69e7702285b586f23b4ff393e05356aff1b8
michaeloffner <michaeloffner@users.noreply.github.com>
2022-09-06 13:58:16 +0200

    ignore access right exception when trying to collect regions

--- source/java/src/org/lucee/extension/resource/s3/S3.java
+++ source/java/src/org/lucee/extension/resource/s3/S3.java
@@ -153,6 +153,7 @@ public class S3 {
 	}
 
 	public String getAccessKeyId() {
+		com.amazonaws.util.AWSRequestMetrics d;
 		return accessKeyId;
 	}
 
@@ -2410,8 +2411,10 @@ public class S3 {
 				}
 			}
 			catch (S3Exception e1) {
-				if (log != null) log.error("s3", e1);
-				else e1.printStackTrace();
+				if ("AccessDenied".equalsIgnoreCase(e1.getErrorCode())) { // in case we can not the region because of access right, we don't care.
+					if (log != null) log.error("s3", e1);
+					else e1.printStackTrace();
+				}
 			}
 			finally {
 				try {
